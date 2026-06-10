914048d56a49941199d4768f9c983072d4968108
Maksim Litvinov <lmk478shade@gmail.com>
2026-06-10 23:07:20 +0300

    fix: server header 2

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://delivery.hosfatantabolis.ru",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
@@ -135,7 +135,7 @@ global.sendToAll = (event, data) => {
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://delivery.hosfatantabolis.ru",
     credentials: true,
   }),
 );
